358c82e6a8eb6316f594e1b7cf0a326e11f277eb
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-11-12 15:09:05 -0500

    App completed and deployed

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,7 +6,7 @@ export class DataService {
   questions: Question[];
 
   constructor() {
-    this.questions = [
+    /*this.questions = [      
       {
         text: 'What is your name?',
         answer: 'My name is Gavin',
@@ -23,10 +23,50 @@ export class DataService {
         hide: true
       }
     ];
+    */
    }
 
+   // Get Questions from LS
    getQuestions() {
+     if (localStorage.getItem('questions') === null) {
+        this.questions = [];
+     } else {
+        this.questions = JSON.parse(localStorage.getItem('questions'));
+     }
+
      return this.questions;
+   }
+
+   // Add Question to LS
+   addQuestion(question: Question) {
+      this.questions.unshift(question);
+
+      // Init local var
+      let questions;
+
+      if (localStorage.getItem('questions') === null) {
+        questions = [];
+        // Push new question
+        questions.unshift(question);
+        // Set new array to LS
+        localStorage.setItem('questions', JSON.stringify(questions));
+     } else {
+       questions = JSON.parse(localStorage.getItem('questions'));
+       // Add new question
+       questions.unshift(question);
+       // Re-set LS
+       localStorage.setItem('questions', JSON.stringify(questions));
+     }
+   }
+
+   // Remove Question from LS
+   removeQuestion(question: Question) {
+     for (let i = 0; i < this.questions.length; i++) {
+       if (this.questions[i] === question) {
+         this.questions.splice(i, 1);
+         localStorage.setItem('questions', JSON.stringify(this.questions));
+       }
+     }
    }
 
 }
